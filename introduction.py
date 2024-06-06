385f5a4d35f442cff61282618de67dafe85d3b26
KipkorirNovestus <kipkorirnovestus1@gmail.com>
2024-06-06 21:55:17 +0300

    first commit

---
+++ introduction.py
@@ -0,0 +1,19 @@
+#rint("Welcome Home")
+#rint("This is my new python project")
+#ullName = "Novestus Kipkorir"
+#ge = "21"
+#rint(fullName)
+#rint (fullName + "-" + age)
+#me ="kipkorir"
+#age = 21
+#height = 5.1
+#maritalStatus = True
+#print(type(name))
+#print(type(age))
+#print(type(maritalStatus))
+#print(type(height))
+print("What is your Name?")
+firstName = input()
+print("How old are you?")
+age=input()
+print("Hello" + " " + firstName + " you are just " + age + " years Old")
